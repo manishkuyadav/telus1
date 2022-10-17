9e7032967e18229a9aa714b607c1e3b8fe31c131
manishkuyadav <104345514+manishkuyadav@users.noreply.github.com>
2022-10-17 19:53:05 +0530

    first commit

---
+++ src/Demo/com/demo2.java
@@ -0,0 +1,23 @@
+package Demo.com;
+
+public class demo2 {
+	
+		public static void main(String[] args)
+		{
+			// initial counter variable
+			int i = 0;
+
+			do {
+
+				// Body of loop that will execute minimum
+				// 1 time for sure no matter what
+				System.out.println("Welcome");
+				i++;
+			}
+
+			
+			while (i < 0);
+		}
+	}
+
+
